87646839863ccfc3b0465e41ae9fafc186eb0c8d
Fatih Arapoglu <f3tt4n@gmail.com>
2023-03-18 12:35:06 +0300

    add 404

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import AllPosts from "./components/AllPosts";
+import NotFound from "./components/NotFound";
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
             <Routes>
                 <Route path="/blog-cms/" element={<Home />} />
                 <Route path="/blog-cms/all" element={<AllPosts />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
     );
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+    return (
+        <div className="error">
+            <h1>Page not found.</h1>
+            <Link to={"/blog-cms/"}>Back to main page</Link>
+        </div>
+    );
+};
+
+export default NotFound;
